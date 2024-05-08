44ee611031f2d77dba2633b2eb85f0adec58a59b
rahim mahmoudzadeh <rahim.mahmoudzadehh@gmail.com>
2024-05-08 15:33:32 +0330

    update library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
     libs.plugins.run{
         alias(androidApplication) apply false
-        alias(google.services) apply false
-        alias(firebase.crashlytics) apply false
         alias(com.android.library) apply false
         alias(kotlinAndroid) apply false
         alias(hilt.plugin) apply false
         alias(ksp) apply false
         alias(kotlin.parcelize) apply false
         alias(androidx.room) apply false
+        alias(google.services) apply false
+        alias(firebase.crashlytics) apply false
     }
 }
