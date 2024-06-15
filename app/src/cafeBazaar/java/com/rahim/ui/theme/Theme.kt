57c73e410bd8d5220ab96ef2e2b2601fa5da38bc
rahim mahmoudzadeh <rahim.mahmoudzadehh@gmail.com>
2024-06-15 20:03:58 +0330

    fix:status bar color cafe bazaar version

--- app/src/cafeBazaar/java/com/rahim/ui/theme/Theme.kt
+++ app/src/cafeBazaar/java/com/rahim/ui/theme/Theme.kt
@@ -1,11 +1,17 @@
 package com.rahim.ui.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Color.White,
@@ -52,6 +58,15 @@ fun YadinoTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+    val view = LocalView.current
+    val color= if(darkTheme) BalticSea.toArgb() else Zircon.toArgb()
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = color
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme // Optional: Set status bar icons to light or dark
+        }
     }
 
     MaterialTheme(
