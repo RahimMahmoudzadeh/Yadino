06701496a4e2c94d37b4ee1b7d7e2878b5909246
Rahim Mahmoudzadeh <59431205+RahimMahmoudzadeh@users.noreply.github.com>
2024-10-10 18:08:05 +0330

    #38 feat welcome MVI

--- feature/welcome/src/main/java/com/rahim/yadino/welcome/WelcomeScreen.kt
+++ feature/welcome/src/main/java/com/rahim/yadino/welcome/WelcomeScreen.kt
@@ -33,6 +33,7 @@ import androidx.navigation.NavHostController
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
+import com.rahim.yadino.base.use
 import com.rahim.yadino.designsystem.component.GradientButton
 import com.rahim.yadino.designsystem.component.gradientColors
 import com.rahim.yadino.designsystem.theme.YadinoTheme
@@ -43,170 +44,178 @@ import kotlinx.coroutines.launch
 
 @Composable
 internal fun WelcomeRoute(
-    modifier: Modifier = Modifier,
-    viewModel: WelcomeViewModel = hiltViewModel(),
-    navigateToHome: () -> Unit
+  modifier: Modifier = Modifier,
+  viewModel: WelcomeViewModel = hiltViewModel(),
+  navigateToHome: () -> Unit,
 ) {
-    WelcomeScreens(
-        modifier = modifier,
-        isShowWelcome = viewModel.isShowWelcomeScreen(),
-        navigateToHome = navigateToHome,
-        saveShowWelcome = viewModel::saveShowWelcome
-    )
+
+  val (state, event) = use(viewModel)
+
+  WelcomeScreens(
+    modifier = modifier,
+    state = state,
+    navigateToHome = navigateToHome,
+    saveShowWelcome = {
+      event.invoke(WelcomeContract.WelcomeEvent.SaveShowWelcome(it))
+    },
+  )
 }
 
 @Composable
 private fun WelcomeScreens(
-    modifier: Modifier = Modifier,
-    isShowWelcome: Boolean,
-    navigateToHome: () -> Unit,
-    saveShowWelcome: (Boolean) -> Unit,
+  modifier: Modifier = Modifier,
+  state: WelcomeContract.WelcomeState,
+  navigateToHome: () -> Unit,
+  saveShowWelcome: (Boolean) -> Unit,
 ) {
-    val scope = rememberCoroutineScope()
-    val pagerState = rememberPagerState()
-    val clickNext = remember { mutableStateOf(isShowWelcome) }
-    var create by rememberSaveable { mutableStateOf(false) }
-    LaunchedEffect(key1 = true) {
-        if (clickNext.value) {
-            navigateToHome()
-            create = false
-            return@LaunchedEffect
-        } else {
-            create = true
-        }
+  val scope = rememberCoroutineScope()
+  val pagerState = rememberPagerState()
+  val clickNext = remember { mutableStateOf(state.isShowedWelcome) }
+  var create by rememberSaveable { mutableStateOf(false) }
+  LaunchedEffect(key1 = true) {
+    if (clickNext.value) {
+      navigateToHome()
+      create = false
+      return@LaunchedEffect
+    } else {
+      create = true
     }
-    if (create) {
-        val listItemWelcome = listOf(
-            WelcomeScreenModel(
-                stringResource(id = R.string.hello),
-                stringResource(id = R.string.welcome_yadino),
-                stringResource(id = R.string.next),
-                R.drawable.welcome1
-            ),
-            WelcomeScreenModel(
-                stringResource(id = R.string.welcome_2),
-                stringResource(id = R.string.welcome_help),
-                stringResource(
-                    id =
-                    R.string.next
-                ),
-                R.drawable.welcome2
-            ),
-            WelcomeScreenModel(
-                stringResource(id = R.string.yadino_life),
-                stringResource(id = R.string.energetic_yadino),
-                stringResource(id = R.string.lets_go),
-                R.drawable.welcome3
-            )
+  }
+  if (create) {
+    val listItemWelcome = listOf(
+      WelcomeScreenModel(
+        stringResource(id = R.string.hello),
+        stringResource(id = R.string.welcome_yadino),
+        stringResource(id = R.string.next),
+        R.drawable.welcome1,
+      ),
+      WelcomeScreenModel(
+        stringResource(id = R.string.welcome_2),
+        stringResource(id = R.string.welcome_help),
+        stringResource(
+          id =
+          R.string.next,
+        ),
+        R.drawable.welcome2,
+      ),
+      WelcomeScreenModel(
+        stringResource(id = R.string.yadino_life),
+        stringResource(id = R.string.energetic_yadino),
+        stringResource(id = R.string.lets_go),
+        R.drawable.welcome3,
+      ),
+    )
+    Column(modifier = modifier) {
+      HorizontalPager(
+        modifier = Modifier.fillMaxHeight(0.87f),
+        count = 3,
+        state = pagerState,
+      ) { page ->
+        WelcomePage(
+          textWelcomeTop = listItemWelcome[page].textWelcomeTop,
+          textWelcomeBottom = listItemWelcome[page].textWelcomeBottom,
+          imageRes = listItemWelcome[page].imageRes,
         )
-        Column(modifier = modifier) {
-            HorizontalPager(
-                modifier = Modifier.fillMaxHeight(0.87f),
-                count = 3,
-                state = pagerState
-            ) { page ->
-                WelcomePage(
-                    textWelcomeTop = listItemWelcome[page].textWelcomeTop,
-                    textWelcomeBottom = listItemWelcome[page].textWelcomeBottom,
-                    imageRes = listItemWelcome[page].imageRes,
-                )
+      }
+      CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+        GradientButton(
+          text = listItemWelcome[pagerState.currentPage].textButton,
+          gradient = Brush.horizontalGradient(gradientColors),
+          modifier = Modifier
+            .padding(top = 28.dp, end = 32.dp, start = 32.dp, bottom = 8.dp),
+          textSize = 18.sp,
+          onClick = {
+            scope.launch {
+              if (pagerState.currentPage == 2) {
+                saveShowWelcome(true)
+                clickNext.value = true
+                navigateToHome()
+              }
+              pagerState.scrollToPage(pagerState.currentPage.plus(1))
             }
-            CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
-                GradientButton(
-                    text = listItemWelcome[pagerState.currentPage].textButton,
-                    gradient = Brush.horizontalGradient(gradientColors),
-                    modifier = Modifier
-                        .padding(top = 28.dp, end = 32.dp, start = 32.dp, bottom = 8.dp),
-                    textSize = 18.sp,
-                    onClick = {
-                        scope.launch {
-                            if (pagerState.currentPage == 2) {
-                                saveShowWelcome(true)
-                                clickNext.value = true
-                                navigateToHome()
-                            }
-                            pagerState.scrollToPage(pagerState.currentPage.plus(1))
-                        }
-                    }
-                )
-            }
-        }
+          },
+        )
+      }
     }
+  }
 }
 
 @Composable
 fun WelcomePage(
-    modifier: Modifier = Modifier,
-    textWelcomeTop: String,
-    textWelcomeBottom: String,
-    imageRes: Int
+  modifier: Modifier = Modifier,
+  textWelcomeTop: String,
+  textWelcomeBottom: String,
+  imageRes: Int,
 ) {
-    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
-        Column(
-            modifier = modifier,
-            horizontalAlignment = CenterHorizontally
-        ) {
-            Image(
-                modifier = Modifier.weight(1f, fill = false),
-                contentScale = ContentScale.FillWidth,
-                painter = painterResource(id = imageRes),
-                contentDescription = "welcomeImage"
-            )
-            Text(
-                text = textWelcomeTop, style = TextStyle(
-                    brush = Brush.verticalGradient(
-                        colors = gradientColors
-                    ), fontWeight = FontWeight.Bold
-                ), fontSize = 26.sp, modifier = Modifier.padding(top = 6.dp)
-            )
-            Text(
-                text = textWelcomeBottom,
-                fontSize = 23.sp,
-                modifier = Modifier
-                    .padding(top = 18.dp, start = 12.dp, end = 12.dp),
-                textAlign = TextAlign.Center,
-                style = TextStyle(
-                    fontWeight = FontWeight.Bold,
-                    color = MaterialTheme.colorScheme.primary
-                )
-            )
+  CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+    Column(
+      modifier = modifier,
+      horizontalAlignment = CenterHorizontally,
+    ) {
+      Image(
+        modifier = Modifier.weight(1f, fill = false),
+        contentScale = ContentScale.FillWidth,
+        painter = painterResource(id = imageRes),
+        contentDescription = "welcomeImage",
+      )
+      Text(
+        text = textWelcomeTop,
+        style = TextStyle(
+          brush = Brush.verticalGradient(
+            colors = gradientColors,
+          ),
+          fontWeight = FontWeight.Bold,
+        ),
+        fontSize = 26.sp, modifier = Modifier.padding(top = 6.dp),
+      )
+      Text(
+        text = textWelcomeBottom,
+        fontSize = 23.sp,
+        modifier = Modifier
+          .padding(top = 18.dp, start = 12.dp, end = 12.dp),
+        textAlign = TextAlign.Center,
+        style = TextStyle(
+          fontWeight = FontWeight.Bold,
+          color = MaterialTheme.colorScheme.primary,
+        ),
+      )
 
-        }
     }
+  }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFFFFF, device = Devices.PIXEL_4)
 @Composable
 private fun WelcomePreview1() {
-    YadinoTheme() {
-        WelcomePage(
-            textWelcomeTop = "سلااام",
-            textWelcomeBottom = "!به خانواده یادینو خوش آمدید",
-            imageRes = com.rahim.yadino.feature.welcome.R.drawable.welcome1,
-        )
-    }
+  YadinoTheme() {
+    WelcomePage(
+      textWelcomeTop = "سلااام",
+      textWelcomeBottom = "!به خانواده یادینو خوش آمدید",
+      imageRes = com.rahim.yadino.feature.welcome.R.drawable.welcome1,
+    )
+  }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFFFFF, device = Devices.PIXEL_4)
 @Composable
 private fun WelcomePreview2() {
-    YadinoTheme() {
-        WelcomePage(
-            textWelcomeTop = "! با یادینو دیگه ازکارات عقب نمیفتی",
-            textWelcomeBottom = "اینجا ما بهت کمک میکنیم تا به همه هدفگذاری هات برسی",
-            imageRes = R.drawable.welcome2,
-        )
-    }
+  YadinoTheme() {
+    WelcomePage(
+      textWelcomeTop = "! با یادینو دیگه ازکارات عقب نمیفتی",
+      textWelcomeBottom = "اینجا ما بهت کمک میکنیم تا به همه هدفگذاری هات برسی",
+      imageRes = R.drawable.welcome2,
+    )
+  }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFFFFF, device = Devices.PIXEL_4)
 @Composable
 private fun WelcomePreview3() {
-    YadinoTheme() {
-        WelcomePage(
-            textWelcomeTop = "!یادینو اپلیکیشنی برای زندگی بهتر",
-            textWelcomeBottom = "با یادینو بانشاط تر منظم تر و هوشمندتر باشید",
-            imageRes = R.drawable.welcome3,
-        )
-    }
+  YadinoTheme() {
+    WelcomePage(
+      textWelcomeTop = "!یادینو اپلیکیشنی برای زندگی بهتر",
+      textWelcomeBottom = "با یادینو بانشاط تر منظم تر و هوشمندتر باشید",
+      imageRes = R.drawable.welcome3,
+    )
+  }
 }
--- feature/welcome/src/main/java/com/rahim/yadino/welcome/WelcomeViewModel.kt
+++ feature/welcome/src/main/java/com/rahim/yadino/welcome/WelcomeViewModel.kt
@@ -2,17 +2,37 @@ package com.rahim.yadino.welcome
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.rahim.yadino.base.BaseViewModel
 import com.rahim.yadino.sharedPreferences.SharedPreferencesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+
 @HiltViewModel
 class WelcomeViewModel @Inject constructor(
-    private val sharedPreferencesRepository: SharedPreferencesRepository,
-) : ViewModel() {
-    fun saveShowWelcome(isShow: Boolean) {
-        sharedPreferencesRepository.saveShowWelcome(isShow)
+  private val sharedPreferencesRepository: SharedPreferencesRepository,
+) : BaseViewModel(), WelcomeContract {
+
+  private val mutableState = MutableStateFlow<WelcomeContract.WelcomeState>(WelcomeContract.WelcomeState())
+  override val state: StateFlow<WelcomeContract.WelcomeState> = mutableState.onStart {
+    isShowWelcomeScreen()
+  }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), WelcomeContract.WelcomeState())
+
+  override fun event(event: WelcomeContract.WelcomeEvent) = when (event) {
+    is WelcomeContract.WelcomeEvent.SaveShowWelcome -> {
+      saveShowWelcome(event.isShow)
     }
-    fun isShowWelcomeScreen() = sharedPreferencesRepository.isShowWelcomeScreen()
+  }
+
+  private fun saveShowWelcome(isShow: Boolean) {
+    sharedPreferencesRepository.saveShowWelcome(isShow)
+  }
+
+  private fun isShowWelcomeScreen() = sharedPreferencesRepository.isShowWelcomeScreen()
 
 }
