4ae62f82d344c80aa66b10d6c22897fd3c648b6c
Rahim Mahmoudzadeh <rahim.mahmoudzadehh@gmail.com>
2025-07-15 09:28:32 +0330

    feat:note data module

---
+++ note/data/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+  libs.plugins.yadino.run {
+    alias(android.library)
+    alias(android.hilt)
+    alias (android.room)
+  }
+}
+
+android {
+  namespace = "com.rahim.yadino.note.data"
+}
+dependencies {
+  projects.run {
+    implementation(core.base)
+  }
+}
---
+++ note/data/src/main/java/com/rahim/yadino/note/data/MyClass.kt
@@ -0,0 +1,4 @@
+package com.rahim.yadino.note.data
+
+class MyClass {
+}
