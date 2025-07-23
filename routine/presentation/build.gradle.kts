fd344b65d0ad1207faa05f6b7f553e737a39df95
Rahim Mahmoudzadeh <59431205+RahimMahmoudzadeh@users.noreply.github.com>
2025-07-23 10:09:48 +0330

    #261 add routine presentation module

---
+++ routine/presentation/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+  libs.plugins.yadino.run {
+    alias(android.feature)
+    alias(android.library.compose)
+  }
+}
+
+android {
+  namespace = "com.rahim.yadino.routine.presentation"
+}
+dependencies {
+  projects.run{
+    implementation(home.domain)
+  }
+
+  libs.run {
+  }
+}
---
+++ routine/presentation/src/main/kotlin/com/yadino/routine/presentation/MyClass.kt
@@ -0,0 +1,4 @@
+package com.yadino.routine.presentation
+
+class MyClass {
+}
