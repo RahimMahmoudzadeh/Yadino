54862b144f8ea6c44ebcea095bc0241b32283f6f
Rahim Mahmoudzadeh <59431205+RahimMahmoudzadeh@users.noreply.github.com>
2025-07-23 10:03:11 +0330

    #261 add routine data module

---
+++ routine/data/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+  libs.plugins.yadino.run {
+    alias(android.library)
+    alias(android.hilt)
+    alias (android.room)
+  }
+}
+
+android {
+  namespace = "com.rahim.yadino.routine.data"
+}
+dependencies {
+  projects.run {
+    implementation(core.base)
+    implementation(core.db)
+    implementation(home.domain)
+  }
+}
---
+++ routine/data/src/main/kotlin/com/yadino/routine/data/MyClass.kt
@@ -0,0 +1,4 @@
+package com.yadino.routine.data
+
+class MyClass {
+}
