8daf164e1fde797702fe8f82cce6dc57294ce5e9
Rahim Mahmoudzadeh <59431205+RahimMahmoudzadeh@users.noreply.github.com>
2025-10-17 15:17:54 +0330

    #284 refactor(designsystem): Simplify `ErrorDialog` implementation
    This commit refactors the `ErrorDialog` composable for better clarity and simplification.
    
    **Key Changes:**
    
    *   Removed the `isOpen` parameter, leaving visibility control to the caller.
    *   Replaced `Surface` with a `Column` that uses `clip` and `background` modifiers for the dialog's appearance.
    *   Simplified the overall structure by removing a layer of nesting.

--- library/designsystem/src/main/kotlin/com/rahim/yadino/designsystem/dialog/ErrorDialog.kt
+++ library/designsystem/src/main/kotlin/com/rahim/yadino/designsystem/dialog/ErrorDialog.kt
@@ -1,5 +1,6 @@
 package com.rahim.yadino.designsystem.dialog
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -15,6 +17,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -30,12 +33,10 @@ import com.rahim.yadino.library.designsystem.R
 @Composable
 fun ErrorDialog(
   modifier: Modifier = Modifier,
-  isOpen: Boolean,
   message: String,
   okMessage: String,
   isClickOk: (Boolean) -> Unit,
 ) {
-  if (!isOpen) return
   BasicAlertDialog(
     properties = DialogProperties(
       usePlatformDefaultWidth = false,
@@ -51,57 +52,54 @@ fun ErrorDialog(
       ),
     onDismissRequest = { isClickOk(false) },
   ) {
-    Surface(
-      shape = RoundedCornerShape(8.dp),
-      color = MaterialTheme.colorScheme.background,
+    Column(
+      modifier = Modifier
+        .fillMaxWidth()
+        .clip(RoundedCornerShape(8.dp))
+        .background(color = MaterialTheme.colorScheme.background),
+      verticalArrangement = Arrangement.Center,
     ) {
-      Column(
-        modifier = Modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.Center,
+      Text(
+        fontSize = 18.sp,
+        modifier = Modifier
+          .fillMaxWidth()
+          .padding(top = 30.dp, end = 50.dp, start = 50.dp),
+        text = message,
+        textAlign = TextAlign.Center,
+        color = MaterialTheme.colorScheme.primary,
+      )
+      Row(
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier
+          .fillMaxWidth()
+          .padding(
+            top = 30.dp,
+            bottom = 30.dp,
+          ),
       ) {
-        Text(
-          fontSize = 18.sp,
-          modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 30.dp, end = 50.dp, start = 50.dp),
-          text = message,
-          textAlign = TextAlign.Center,
-          color = MaterialTheme.colorScheme.primary,
+        DialogButtonBorder(
+          text = stringResource(id = R.string.no),
+          gradient = Brush.verticalGradient(gradientColors),
+          modifier = Modifier,
+          textSize = 14.sp,
+          width = 0.22f,
+          40.dp,
+          onClick = {
+            isClickOk(false)
+          },
         )
-        Row(
-          horizontalArrangement = Arrangement.Center,
+        DialogButtonBackground(
+          text = okMessage,
+          gradient = Brush.verticalGradient(gradientColors),
           modifier = Modifier
-            .fillMaxWidth()
-            .padding(
-              top = 30.dp,
-              bottom = 30.dp,
-            ),
-        ) {
-          DialogButtonBorder(
-            text = stringResource(id = R.string.no),
-            gradient = Brush.verticalGradient(gradientColors),
-            modifier = Modifier,
-            textSize = 14.sp,
-            width = 0.22f,
-            40.dp,
-            onClick = {
-              isClickOk(false)
-            },
-          )
-
-          DialogButtonBackground(
-            text = okMessage,
-            gradient = Brush.verticalGradient(gradientColors),
-            modifier = Modifier
-              .fillMaxWidth(0.3f)
-              .height(40.dp)
-              .padding(start = 16.dp),
-            textSize = 14.sp,
-            onClick = {
-              isClickOk(true)
-            },
-          )
-        }
+            .fillMaxWidth(0.3f)
+            .height(40.dp)
+            .padding(start = 16.dp),
+          textSize = 14.sp,
+          onClick = {
+            isClickOk(true)
+          },
+        )
       }
     }
   }
