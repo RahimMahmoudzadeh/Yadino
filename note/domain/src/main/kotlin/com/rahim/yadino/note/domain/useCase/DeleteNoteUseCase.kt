38773338962591af4b4723fa9423bbb1c2671d2f
Rahim Mahmoudzadeh <59431205+RahimMahmoudzadeh@users.noreply.github.com>
2025-10-23 20:47:34 +0330

    #293 feat(note-domain): Add `DeleteNoteUseCase`
    This commit introduces a new use case, `DeleteNoteUseCase`, to the `note/domain` module.
    
    The use case encapsulates the business logic for deleting a note. It takes a `NoteRepository` as a dependency and provides an `invoke` operator that delegates the deletion operation to the repository.

---
+++ note/domain/src/main/kotlin/com/rahim/yadino/note/domain/useCase/DeleteNoteUseCase.kt
@@ -0,0 +1,10 @@
+package com.rahim.yadino.note.domain.useCase
+
+import com.rahim.yadino.note.domain.NoteRepository
+import com.rahim.yadino.note.domain.model.Note
+
+class DeleteNoteUseCase(private val noteRepository: NoteRepository) {
+  suspend operator fun invoke(note: Note) {
+    noteRepository.deleteNote(note)
+  }
+}
