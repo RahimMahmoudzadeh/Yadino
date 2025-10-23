01d009b9d495ddb7bf65df5ae40454113212b16c
Rahim Mahmoudzadeh <59431205+RahimMahmoudzadeh@users.noreply.github.com>
2025-10-23 20:28:38 +0330

    #287 feat(home): Register domain use cases in Koin module
    This commit updates the Koin dependency injection module for the `home` feature to register all necessary domain use cases.
    
    The `HomeDiModule` now provides single instances of the following use cases, making them available for injection throughout the feature:
    *   `AddReminderUseCase`
    *   `UpdateReminderUseCase`
    *   `CancelReminderUseCase`
    *   `DeleteReminderUseCase`
    *   `GetTodayRoutinesUseCase`
    *   `SearchRoutineUseCase`
    *   `GetCurrentDateUseCase`
    
    Additionally, the module `homeDiModule` has been renamed to `HomeDiModule` to follow Kotlin's naming conventions.

--- home/data/src/main/kotlin/com/rahim/yadino/home/data/di/HomeModule.kt
+++ home/data/src/main/kotlin/com/rahim/yadino/home/data/di/HomeModule.kt
@@ -1,9 +1,23 @@
 package com.rahim.yadino.home.data.di
 
 import com.rahim.home.domain.repo.HomeRepository
+import com.rahim.home.domain.useCase.AddReminderUseCase
+import com.rahim.home.domain.useCase.CancelReminderUseCase
+import com.rahim.home.domain.useCase.DeleteReminderUseCase
+import com.rahim.home.domain.useCase.GetCurrentDateUseCase
+import com.rahim.home.domain.useCase.GetTodayRoutinesUseCase
+import com.rahim.home.domain.useCase.SearchRoutineUseCase
+import com.rahim.home.domain.useCase.UpdateReminderUseCase
 import com.rahim.yadino.home.data.repoImpl.HomeRepositoryImpl
 import org.koin.dsl.module
 
-val homeDiModule = module {
+val HomeDiModule = module {
   single<HomeRepository> { HomeRepositoryImpl(routineDao = get(), sharedPreferencesRepository = get()) }
+  single { AddReminderUseCase(routineRepository = get(), reminderScheduler = get()) }
+  single { UpdateReminderUseCase(routineRepository = get(), reminderScheduler = get()) }
+  single { CancelReminderUseCase(routineRepository = get(), reminderScheduler = get()) }
+  single { GetTodayRoutinesUseCase(routineRepository = get()) }
+  single { SearchRoutineUseCase(routineRepository = get()) }
+  single { GetCurrentDateUseCase(homeRepository = get()) }
+  single { DeleteReminderUseCase(routineRepository = get(), reminderScheduler = get()) }
 }
