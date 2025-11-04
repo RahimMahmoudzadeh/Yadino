f2f897c6ba035f8ec45223f43356b70fc21a7f78
Rahim Mahmoudzadeh <rahim.mahmoudzadehh@gmail.com>
2025-11-04 11:33:14 +0330

    #288 refactor(history): Extend UnidirectionalViewModel in RoutineHistoryContract
    This commit updates the `RoutineHistoryContract` interface to extend from `UnidirectionalViewModel` instead of `UnidirectionalComponent`.
    
    This change aligns the contract with the broader architectural pattern, likely simplifying its implementation and ensuring consistency with other view models in the codebase.

--- routine/presentation/src/main/kotlin/com/yadino/routine/presentation/history/RoutineHistoryContract.kt
+++ routine/presentation/src/main/kotlin/com/yadino/routine/presentation/history/RoutineHistoryContract.kt
@@ -3,10 +3,11 @@ package com.yadino.routine.presentation.history
 import androidx.compose.runtime.Immutable
 import com.rahim.yadino.base.LoadableData
 import com.rahim.yadino.base.UnidirectionalComponent
+import com.rahim.yadino.base.UnidirectionalViewModel
 import com.rahim.yadino.enums.error.ErrorMessageCode
 import com.yadino.routine.presentation.model.IncompleteOrCompletedRoutinesUiModel
 
-interface RoutineHistoryContract : UnidirectionalComponent<RoutineHistoryContract.HistoryEvent, RoutineHistoryContract.HistoryState> {
+interface RoutineHistoryContract : UnidirectionalViewModel<RoutineHistoryContract.HistoryEvent, RoutineHistoryContract.HistoryState> {
   @Immutable
   sealed class HistoryEvent() {
     data object GetAllRoutine : HistoryEvent()
