4ba4f0e75c97a8efdd9cf93231b44e583d99f06e
Rahim Mahmoudzadeh <59431205+RahimMahmoudzadeh@users.noreply.github.com>
2025-11-07 20:23:32 +0330

    #288 chore(note): Remove NotePresentationDiModule
    This commit removes the `NotePresentationDiModule.kt` file.
    
    The Koin module defined in this file, which provided the `NoteComponentImpl` ViewModel, is no longer needed and has been deleted.
    
    **Key Changes:**
    
    *   **`note/presentation/src/main/kotlin/com/rahim/yadino/note/presentation/di/NotePresentationDiModule.kt`**:
        *   Deleted the entire file.

--- note/presentation/src/main/kotlin/com/rahim/yadino/note/presentation/di/NotePresentationDiModule.kt
+++
@@ -1,9 +0,0 @@
-package com.rahim.yadino.note.presentation.di
-
-import com.rahim.yadino.note.presentation.component.NoteComponentImpl
-import org.koin.core.module.dsl.viewModel
-import org.koin.dsl.module
-
-val NotePresentationDiModule = module {
-  viewModel { NoteComponentImpl(getNotesUseCase = get(), addNoteUseCase = get(), deleteNoteUseCase = get(), updateNoteUseCase = get(), searchNoteUseCase = get()) }
-}
