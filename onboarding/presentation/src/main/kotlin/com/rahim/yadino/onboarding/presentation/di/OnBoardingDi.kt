1a5aeeca37f8c16fbd3751b5daa62876451f0c87
Rahim Mahmoudzadeh <59431205+RahimMahmoudzadeh@users.noreply.github.com>
2025-11-07 20:23:58 +0330

    #288 refactor(onboarding): Remove Koin module for OnBoardingComponent
    This commit removes the `OnBoardingDiModule`, which was responsible for providing the `OnBoardingComponent` using Koin.
    
    This change suggests that the dependency injection setup for the onboarding feature is being either removed or refactored to a different approach.
    
    **Key Changes:**
    
    *   **`OnBoardingDi.kt`**:
        *   The file and its entire content, including the Koin module definition, have been deleted.

--- onboarding/presentation/src/main/kotlin/com/rahim/yadino/onboarding/presentation/di/OnBoardingDi.kt
+++
@@ -1,9 +0,0 @@
-package com.rahim.yadino.onboarding.presentation.di
-
-import com.rahim.yadino.onboarding.presentation.component.OnBoardingComponent
-import org.koin.core.module.dsl.viewModel
-import org.koin.dsl.module
-
-val OnBoardingDiModule = module {
-  viewModel { OnBoardingComponent(get()) }
-}
