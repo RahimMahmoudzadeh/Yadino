3c88e60da6aae84474b69771f7626922b3d624dc
Rahim Mahmoudzadeh <59431205+RahimMahmoudzadeh@users.noreply.github.com>
2025-11-20 11:31:11 +0330

    #304 refactor(home): Change routine alarm ID type from Long to Int
    This commit updates the data type for routine alarm IDs from `Long` to `Int` across the domain and data layers. This change affects the `Routine` model and the `HomeRepository` interface, ensuring consistency in how alarm IDs are generated and stored.
    
    **Key Changes:**
    
    *   **`Routine.kt`**:
        *   Changed the type of `idAlarm` from `Long?` to `Int?`.
    *   **`HomeRepository.kt`**:
        *   Updated the return type of `getRoutineAlarmId()` from `Long` to `Int`.
    *   **`HomeRepositoryImpl.kt`**:
        *   Updated the implementation of `getRoutineAlarmId()` to return an `Int`.
        *   Modified the ID generation logic to use `Random.nextInt` instead of `Random.nextLong`.

--- home/data/src/main/kotlin/com/rahim/yadino/home/data/repoImpl/HomeRepositoryImpl.kt
+++ home/data/src/main/kotlin/com/rahim/yadino/home/data/repoImpl/HomeRepositoryImpl.kt
@@ -73,11 +73,11 @@ class HomeRepositoryImpl(
     }
   }
 
-  override suspend fun getRoutineAlarmId(): Long {
+  override suspend fun getRoutineAlarmId(): Int {
     val existingAlarmIds = routineDao.getIdAlarms().toSet()
-    var potentialId: Long
+    var potentialId: Int
     do {
-      potentialId = Random.Default.nextLong(1L, Long.MAX_VALUE)
+      potentialId = Random.nextInt(1, Int.MAX_VALUE)
     } while (existingAlarmIds.contains(potentialId))
 
     return potentialId
