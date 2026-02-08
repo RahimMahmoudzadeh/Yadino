2d71675d366fd42996624bcb2e65d79b251e7321
Rahim Mahmoudzadeh <rahim.mahmoudzadehh@gmail.com>
2026-02-08 14:10:13 +0330

    #318 feat(home): Add initial files for ErrorDialog component
    This commit introduces the initial file structure for a new `ErrorDialog` feature within the home presentation module.
    
    It adds the following placeholder files:
    *   `ErrorDialogComponent.kt`: An interface for the component.
    *   `ErrorDialogComponentImpl.kt`: An empty implementation class.
    *   `ErrorDialogUi.kt`: An empty file for the UI.

---
+++ home/presentation/src/main/kotlin/com/rahim/yadino/home/presentation/ui/errorDialog/ErrorDialogUi.kt
@@ -0,0 +1,2 @@
+package com.rahim.yadino.home.presentation.ui.errorDialog
+
---
+++ home/presentation/src/main/kotlin/com/rahim/yadino/home/presentation/ui/errorDialog/component/ErrorDialogComponent.kt
@@ -0,0 +1,4 @@
+package com.rahim.yadino.home.presentation.ui.errorDialog.component
+
+interface ErrorDialogComponent {
+}
